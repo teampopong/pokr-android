f46d8176b0fe184dbb58733ef67d00d1ce36e550
unknown <ato0307>
2013-10-09 14:20:41 +0900

    wrapping the pork mobile webpage by webview

--- src/kr/pork/android/MainActivity.java
+++ src/kr/pork/android/MainActivity.java
@@ -1,22 +1,37 @@
 package kr.pork.android;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+	
+	private WebView mWebView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mWebView = (WebView) findViewById(R.id.webview);
+		 // 웹뷰에서 자바스크립트실행가능
+        mWebView.getSettings().setJavaScriptEnabled(true); 
+        // 구글홈페이지 지정
+        mWebView.loadUrl("http://pokr.kr");
+        // WebViewClient 지정
+        mWebView.setWebViewClient(new WebViewClientClass());  
+         
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
+    private class WebViewClientClass extends WebViewClient { 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
+            view.loadUrl(url); 
+            return true; 
+        } 
+    }
+
 
 }
