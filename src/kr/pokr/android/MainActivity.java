c556627fd19ef70ee888db22c64c3f774da7d1f4
stay.leone <stray.leone@gmail.com>
2013-10-19 00:33:50 +0900

    on back key down, come back to the previous page instead of closing app

--- src/kr/pokr/android/MainActivity.java
+++ src/kr/pokr/android/MainActivity.java
@@ -2,6 +2,7 @@ package kr.pokr.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -22,6 +23,18 @@ public class MainActivity extends Activity {
         // set a WebViewClient
         mWebView.setWebViewClient(new WebViewClientClass());  
          
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    // Check if the key event was the Back button and if there's history
+	    if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+	    	mWebView.goBack();
+	        return true;
+	    }
+	    // If it wasn't the Back key or there's no web page history, bubble up to the default
+	    // system behavior (probably exit the activity)
+	    return super.onKeyDown(keyCode, event);
 	}
 
 
