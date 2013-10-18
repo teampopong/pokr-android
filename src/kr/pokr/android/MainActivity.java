a33f0fe8b7929b0dc41344314802a0e75fc21f6f
stay.leone <stray.leone@gmail.com>
2013-10-19 01:04:59 +0900

    add the loading progressbar

--- src/kr/pokr/android/MainActivity.java
+++ src/kr/pokr/android/MainActivity.java
@@ -3,28 +3,35 @@ package kr.pokr.android;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	
+
 	private WebView mWebView;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		mWebView = (WebView) findViewById(R.id.webview);
+		progressBar = (ProgressBar) this.findViewById(R.id.progress);
 		// enable java sciript
-        mWebView.getSettings().setJavaScriptEnabled(true); 
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        // set a WebViewClient
+        mWebView.setWebViewClient(new WebViewClientClass());
+        mWebView.setWebChromeClient(new WebChromeClientClass());
         // designate the URL to load
         mWebView.loadUrl("http://pokr.kr");
-        // set a WebViewClient
-        mWebView.setWebViewClient(new WebViewClientClass());  
-         
+
 	}
-	
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 	    // Check if the key event was the Back button and if there's history
@@ -38,13 +45,35 @@ public class MainActivity extends Activity {
 	}
 
 
-    private class WebViewClientClass extends WebViewClient { 
+    private class WebViewClientClass extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
-            view.loadUrl(url); 
-            return true; 
-        } 
-    }
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
 
+		public void onPageStarted(WebView view, String url,
+				android.graphics.Bitmap favicon) {
+			super.onPageStarted(view, url, favicon);
+			progressBar.setVisibility(View.VISIBLE);
+		}
+
+		public void onPageFinished(WebView view, String url) {
+			super.onPageFinished(view, url);
+			progressBar.setVisibility(View.INVISIBLE);
+		}
+		public void onReceivedError(WebView view, int errorCode,
+				String description, String failingUrl) {
+			super.onReceivedError(view, errorCode, description, failingUrl);
+			Toast.makeText(MainActivity.this, "Fail to load" + description,
+					Toast.LENGTH_SHORT).show();
+		}
+    }
+    private class WebChromeClientClass extends WebChromeClient {
+		@Override
+		public void onProgressChanged(WebView view, int newProgress) {
+			progressBar.setProgress(newProgress);
+		}
+	}
 
 }
