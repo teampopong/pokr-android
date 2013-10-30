cec80a044aa75fcd7b2cca7a3940d8f29577a1f1
stray-leone <stray.leone@gmail.com>
2013-10-31 03:39:01 +0900

    not called super.onDestroy()

--- src/kr/pokr/android/MainActivity.java
+++ src/kr/pokr/android/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onDestroy()
 	{
+		super.onDestroy();
 	    unregisterReceiver(statusUpdateIntentReceiver);
 	    unregisterReceiver(messageIntentReceiver);
 
