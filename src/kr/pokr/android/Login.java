d2f4562b9ce2659c5c913dd31a22de269eb13f32
hyunho.kim <stray.leone@mail.com>
2014-09-07 20:54:08 +0900

    change login layout to fragment layout

--- src/kr/pokr/android/Login.java
+++ src/kr/pokr/android/Login.java
@@ -1,9 +1,11 @@
 package kr.pokr.android;
 
-import android.content.Intent;
+import kr.pokr.android.fragment.LoginFragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 
 /**
  * The Class Login is an Activity class that shows the login screen to users.
@@ -13,6 +15,7 @@ import android.view.View;
 public class Login extends FragmentActivity
 {
 
+	int mCurrentFragmentIndex;
 	/* (non-Javadoc)
 	 * @see com.chatt.custom.CustomActivity#onCreate(android.os.Bundle)
 	 */
@@ -21,23 +24,47 @@ public class Login extends FragmentActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		mCurrentFragmentIndex = 0;
 
-		//setTouchNClick(R.id.btnLogin);
-		//setTouchNClick(R.id.btnFb);
+		fragmentReplace(mCurrentFragmentIndex);
 	}
+	
+	public void fragmentReplace(int reqNewFragmentIndex) {
 
-	/* (non-Javadoc)
-	 * @see com.chatt.custom.CustomActivity#onClick(android.view.View)
-	 */
-	/*@Override
-	public void onClick(View v)
-	{
-		super.onClick(v);
-		if (v.getId() != R.id.btnReg)
-		{
-			startActivity(new Intent(this, MainActivity.class));
-			finish();
+		Fragment newFragment = null;
+
+//		Log.d(TAG, "fragmentReplace " + reqNewFragmentIndex);
+
+		newFragment = getFragment(reqNewFragmentIndex);
+
+		// replace fragment
+		final FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+
+		transaction.replace(R.id.login_fragment, newFragment);
+
+		// Commit the transaction
+		transaction.commit();
+
+	}
+	
+	private Fragment getFragment(int idx) {
+		Fragment newFragment = null;
+
+		switch (idx) {
+		case 0:
+			newFragment = new LoginFragment();
+			break;
+		case 1:
+			newFragment = null;
+			break;
+
+		default:
+//			Log.d(TAG, "Unhandle case");
+			break;
 		}
+
+		return newFragment;
 	}
-	*/
+
 }
